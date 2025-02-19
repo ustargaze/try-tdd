8a2e9419a3488d8eba364ea21974fde382a3d630
u <31676530+ustargaze@users.noreply.github.com>
2025-02-20 00:26:49 +0800

    feat: division and multiplication features done.

--- go/money_test.go
+++ go/money_test.go
@@ -2,20 +2,45 @@ package main
 
 import "testing"
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
-	}
-	tenner := fiver.Times(2)
-	if tenner.amount != 10 {
-		t.Errorf("Excepted 10, got: [%d]", tenner.amount)
+func TestMultiplicationDollars(t *testing.T) {
+	money := Money{amount: 5, currency: "USD"}
+	actual := money.Times(2)
+	excepted := Money{amount: 10, currency: "USD"}
+
+	assertEqual(t, excepted, actual)
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	actual := tenEuros.Times(2)
+	excepted := Money{amount: 20, currency: "EUR"}
+
+	assertEqual(t, excepted, actual)
+}
+
+func TestDivision(t *testing.T) {
+	money := Money{amount: 4002, currency: "KRW"}
+	actual := money.Divide(4)
+	excepted := Money{amount: 1000.5, currency: "KRW"}
+
+	assertEqual(t, excepted, actual)
+}
+
+func assertEqual(t *testing.T, excepted Money, actual Money) {
+	if excepted != actual {
+		t.Errorf("Excepted %+v Got: %+v", excepted, actual)
 	}
 }
 
-type Dollar struct {
-	amount int
+type Money struct {
+	amount   float64
+	currency string
 }
 
-func (d *Dollar) Times(multiplier int) *Dollar {
-	return &Dollar{amount: d.amount * multiplier}
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
+}
+
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
 }
